adb69b8d126f7bad15386995f5cabb72c511d686
Clibanez <clibanezzcaldas@gmail.com>
2023-04-11 11:58:17 -0300

    classes entity e enum

---
+++ src/main/java/br/com/projeto/forum/modelo/Curso.java
@@ -0,0 +1,30 @@
+package br.com.projeto.forum.modelo;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Curso {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String categoria;
+
+    public Curso(String nome, String categoria) {
+        this.nome = nome;
+        this.categoria = categoria;
+    }
+}
---
+++ src/main/java/br/com/projeto/forum/modelo/Usuario.java
@@ -0,0 +1,2 @@
+package br.com.projeto.forum.modelo;public class Usuario {
+}
