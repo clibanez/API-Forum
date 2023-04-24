c1c832d029c4588bfea951b45feb648cd3b1c6ca
Clibanez <clibanezzcaldas@gmail.com>
2023-04-24 14:32:30 -0300

    Classe repositories

--- src/main/java/br/com/projeto/forum/repositories/CursoRepository.java
+++ src/main/java/br/com/projeto/forum/repositories/CursoRepository.java
@@ -1,2 +1,8 @@
-package br.com.projeto.forum.repositories;public interface CursoRepository {
+package br.com.projeto.forum.repositories;
+
+import br.com.projeto.forum.modelo.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+    Curso findByNome(String nomeCurso);
 }
